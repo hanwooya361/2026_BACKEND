935744a4a9fe472bbd8526c54b0bb86e52c076ed
hanwooya361 <johunwoo123456@gmail.com>
2026-07-28 11:09:54 +0900

    first commit

---
+++ src/day01/Exam1.java
@@ -0,0 +1,16 @@
+/*
+    .java 파일 만들기
+        1. src 폴더내 우클릭
+        2. new java file -> calss
+        3. 첫글자는 대문자 영문으로 시작하는 클래스명 임의
+    자바 주석 == js 주석
+*/
+
+package day01;  // 패키지(폴더명) 이면서 클래스의 위치 식별용도
+
+public class Exam1 {
+    public static void main(String[] args) {
+     int b = 10;
+     System.out.println("안녕자바");   
+    }
+}
