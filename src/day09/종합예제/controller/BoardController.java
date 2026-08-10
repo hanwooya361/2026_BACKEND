eb2c8f991d6e5cc494f7377e59fafdcd4b978ec8
hanwooya361 <johunwoo123456@gmail.com>
2026-08-10 15:15:25 +0900

    day25 5h

---
+++ src/day09/종합예제/controller/BoardController.java
@@ -0,0 +1,34 @@
+package day09.종합예제.controller;
+
+import java.util.ArrayList;
+
+import day09.종합예제.model.dao.BoardDao;
+import day09.종합예제.model.dto.BoardDto;
+
+public class BoardController {
+    private BoardController(){};
+    private static final BoardController instance = new BoardController();
+    public static BoardController getInstance(){
+        return instance;
+    }
+    private BoardDao bd = BoardDao.getInstance();   // controller 에서 dao 싱글톤 호출
+
+    // [1] 게시물 작성 컨트롤러
+    public boolean save(BoardDto boardDto){
+        // 1. view로 부터 저장할 정보 객체로 받기
+        // * 유효성검사/타입변환 등등
+        // 2. DAO에게 요청하고 응답받기
+        boolean result = bd.save(boardDto);
+        // 3. DAO에게 받은 결과를 view에게 응답하기
+        return result;
+    }
+
+    // [2] 게시물 전체 조회
+    public ArrayList<BoardDto> findAll(){
+        // 1. view에게 매개변수 받기. 현재는 없음
+        // 2. DAO에게 요청하고 응답받기
+        ArrayList<BoardDto> result =  bd.findAll();
+        // 3. DAO에게 받은 결과를 VIEW에게 반환
+        return result;
+    }
+}
---
+++ src/day09/종합예제/model/dao/BoardDao.java
@@ -0,0 +1,33 @@
+package day09.종합예제.model.dao;
+
+import java.util.ArrayList;
+
+import day09.종합예제.model.dto.BoardDto;
+
+public class BoardDao {
+    private BoardDao(){}
+    private static  final BoardDao instance = new BoardDao();
+    public static BoardDao getInstance(){
+        return instance;
+    }
+    // 추후에 MYSQL 서버와 연동     // DB 대신 ArrayList사용
+    private final ArrayList<BoardDto> boardList = new ArrayList<>();
+
+    // [1] 게시물 작성 로직
+    public boolean save(BoardDto boardDto){
+        // 1. 컨트롤러로부터 저장할 정보 객체 받기
+        // * 추후에 insert 이용한 DB에 저장
+        // 2. 리스트에 저장
+        boardList.add(boardDto);
+        // 3. 성공반환
+        return true;
+    }
+
+    // [2] 게시물 전체 조회 로직
+    public ArrayList<BoardDto> findAll(){
+        // 1. 컨트롤러에게 매개변수 받기. 현재는 없음
+        // * 추후에 select 이용한 DB 조회
+        // 2. 리스트 전체 반환
+        return boardList;
+    }
+}
